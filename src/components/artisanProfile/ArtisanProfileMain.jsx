051507f9125239283a8ae4e6e8fe4b34a208d661
blemathegreat <blemathegreat@gmail.com>
2026-07-02 11:27:42 +0100

    update ArtisanMAinProfile

--- src/components/artisanProfile/ArtisanProfileMain.jsx
+++ src/components/artisanProfile/ArtisanProfileMain.jsx
@@ -3,7 +3,7 @@ import { ChevronRight, ArrowLeft } from 'lucide-react'
 import { Artisans } from '../../assets/Photos'
 
 import ProfileHeader     from './ProfileHeader'
-import ArtisanProfileNav from './ArtisanProfileNav'
+import ArtisanProfileNav from './ArtisanProfilenav'
 import ProfileAbout      from './ProfileAbout'
 import ProfileServices   from './ProfileServices'
 import ProfilePortfolio  from './ProfilePortfolio'
